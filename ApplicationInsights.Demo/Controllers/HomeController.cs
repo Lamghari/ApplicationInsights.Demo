dae55337d662f9b04f18bf3be9e118afb9a3e3a9
Mohamed Lamghari <lamghari.mohamed@hotmail.com>
2021-02-04 12:10:20 +0100

    Add log4net

--- ApplicationInsights.Demo/Controllers/HomeController.cs
+++ ApplicationInsights.Demo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,19 @@ namespace ApplicationInsights.Demo.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public ActionResult Index()
         {
+            log.Info("[INDEX] Message object with the log4net.Core.Level.Info Info");
+
             return View();
         }
 
         public ActionResult About()
         {
+            log.Debug("[ABOUT] Message object with the log4net.Core.Level.Debug Debug");
+
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -22,6 +29,8 @@ namespace ApplicationInsights.Demo.Controllers
 
         public ActionResult Contact()
         {
+            log.Error("[CONTACT] Message object with the log4net.Core.Level.Error Error");
+
             ViewBag.Message = "Your contact page.";
 
             return View();
